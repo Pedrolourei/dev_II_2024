db71e0348a6110cde35f00225cf56186b853091b
Pedro Loureiro <pedroseko@gmail.com>
2024-08-27 12:30:07 -0300

    cruds

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -11,11 +11,38 @@ class UserRouter {
 
   private initializeRoutes() {
     this.router.get("/", this.getAllUsers);
+    this.router.post("/", this.createUser);
+    this.router.get("/:id", this.getUserById);
+    this.router.put("/:id", this.updateUser);
+    this.router.delete("/:id", this.removeUser);
   }
 
   private async getAllUsers(req: Request, res: Response) {
     const users = await UserRepository.getUsers();
     res.status(200).json(users);
+  }
+
+  private async createUser(req: Request, res: Response) {
+    const user = await UserRepository.newUser(req.body);
+    res.status(200).json(user);
+  }
+
+  private async getUserById(req: Request, res: Response) {
+    const id = parseInt(req.params.id);
+    const user = await UserRepository.getUserById(id);
+    res.status(200).json(user);
+  }
+
+  private async updateUser(req: Request, res: Response) {
+    const id = parseInt(req.params.id);
+    const user = await UserRepository.updateUser(id, req.body);
+    res.status(201).json(user);
+  }
+
+  private async removeUser(req: Request, res: Response) {
+    const id = parseInt(req.params.id);
+    await UserRepository.removeUser(id);
+    res.status(201).json({ message: "Registro removido com sucesso" });
   }
 }
 
--- src/app/repositories/UserRepository.ts
+++ src/app/repositories/UserRepository.ts
@@ -1,12 +1,32 @@
 import User from "../entities/Users";
 import IUser from "../interfaces/IUser";
 import { AppDataSource } from "../../database/data-source";
+import ErrorExtension from "../utils/ErrorExtension";
 
 class UserRepository {
   private static usersRepository = AppDataSource.getRepository(User);
 
   static async getUsers(): Promise<IUser[]> {
     return this.usersRepository.find();
+  }
+
+  static async newUser(user: IUser): Promise<IUser> {
+    const createdUser = await this.usersRepository.save(user);
+    return createdUser as IUser;
+  }
+
+  static async getUserById(id: number): Promise<IUser | null> {
+    return this.usersRepository.findOneBy({ id });
+  }
+
+  static async updateUser(id: number, user: IUser): Promise<IUser> {
+    await this.usersRepository.update(id, user);
+
+    return user as IUser;
+  }
+
+  static async removeUser(id: number): Promise<void> {
+    await this.usersRepository.delete(id);
   }
 }
 
